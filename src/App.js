abf4a672f38ee0f1b944353adea8386a9d892cc4
Siddharth Rajan <siddharthrajan10@gmail.com>
2019-03-09 23:07:33 -0500

    add given entries

--- src/App.js
+++ src/App.js
@@ -5,21 +5,47 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import ListTodos from './ListTodos/ListTodos.js';
-//import {generate} from 'randomstring';
 
 class App extends Component {
   state = {
-    "todos": [
-      { "key": 1,
-        "title": 'Walk the cat',
-        "description": 'She is going crazy'},
-      { "key": 2,
-        "title": 'Pacify Aliens',
-        "description": 'They don\'t want to hear Halloween jokes anymore'}
-    ],
-    "collapse": false,
-    "formTitle": '',
-    "formDescription": ''
+    "entries": [
+      {
+        FirstName: "Cathy" ,
+        LastName: "Pierce",
+        Birthday: "9/14/1996",
+        Telephone: "200-910-8132"
+      },
+      {
+        FirstName: "Alfonso",
+        LastName: "Cooley",
+        Birthday: "8/10/1973",
+        Telephone: "200-657-9362"
+      },
+      {
+        FirstName: "Victor",
+        LastName: "Gordon",
+        Birthday:  "8/3/1970",
+        Telephone: "200-661-9407"
+      },
+      {
+        FirstName: "Colleen",
+        LastName: "Wright",
+        Birthday: "10/28/1967", 
+        Telephone: "200-250-7949"
+      },
+      {
+        FirstName: "James",
+        LastName: "Johnston",
+        Birthday: "5/11/1972",
+        Telephone: "200-645-3176"
+      },
+      {
+        FirstName: "Anna",
+        LastName: "Reyes",
+        Birthday: "9/10/1975",
+        Telephone: "200-707-8670"
+      }
+    ]
   }
 
   addTodoHandler = (event) => {
