50e720b24f0d71e22a9de2108507f9d97d8e1ac8
Siddharth Rajan <siddharthrajan10@gmail.com>
2019-03-09 23:28:56 -0500

    add quotes in entries, and keys

--- src/App.js
+++ src/App.js
@@ -5,45 +5,52 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import ListTodos from './ListTodos/ListTodos.js';
+import { generate } from 'randomstring'
 
 class App extends Component {
   state = {
     "entries": [
       {
-        FirstName: "Cathy" ,
-        LastName: "Pierce",
-        Birthday: "9/14/1996",
-        Telephone: "200-910-8132"
+        "key": generate(10),
+        "FirstName": "Cathy" ,
+        "LastName": "Pierce",
+        "Birthday": "9/14/1996",
+        "Telephone": "200-910-8132"
       },
       {
-        FirstName: "Alfonso",
-        LastName: "Cooley",
-        Birthday: "8/10/1973",
-        Telephone: "200-657-9362"
+        "key": generate(10),
+        "FirstName": "Alfonso",
+        "LastName": "Cooley",
+        "Birthday": "8/10/1973",
+        "Telephone": "200-657-9362"
       },
       {
-        FirstName: "Victor",
-        LastName: "Gordon",
-        Birthday:  "8/3/1970",
-        Telephone: "200-661-9407"
+        "key": generate(10),
+        "FirstName": "Victor",
+        "LastName": "Gordon",
+        "Birthday":  "8/3/1970",
+        "Telephone": "200-661-9407"
       },
       {
-        FirstName: "Colleen",
-        LastName: "Wright",
-        Birthday: "10/28/1967", 
-        Telephone: "200-250-7949"
+        "key": generate(10),
+        "FirstName": "Colleen",
+        "LastName": "Wright",
+        "Birthday": "10/28/1967", 
+        "Telephone": "200-250-7949"
       },
       {
-        FirstName: "James",
-        LastName: "Johnston",
-        Birthday: "5/11/1972",
-        Telephone: "200-645-3176"
+        "key": generate(10),
+        "FirstName": "James",
+        "LastName": "Johnston",
+        "Birthday": "5/11/1972",
+        "Telephone": "200-645-3176"
       },
       {
-        FirstName: "Anna",
-        LastName: "Reyes",
-        Birthday: "9/10/1975",
-        Telephone: "200-707-8670"
+        "key": generate(10),
+        "FirstName": "Anna",
+        "LastName": "Reyes",
+        "Birthday": "9/10/1975",
+        "Telephone": "200-707-8670"
       }
     ],
     "collapse": false,
